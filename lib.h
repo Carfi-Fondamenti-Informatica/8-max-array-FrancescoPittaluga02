131bfe4491a006df3abeb9af3c768b7c592f535d
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-05-08 17:56:36 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,18 +1,6 @@
-#include "lib.h"
-bool primo(char a){
-  
-  if((a>=65 and a<=90)or(a>=97 and a<=127)){
-    return true;
-  }else{
-    return false;
-  }
-     }
-  char trasformazione(char a){
-    if(a>=65 and a<=90){
-      a=a+32;
-      return a;
-    }else if (a>=97 and a<=127){
-      a=a-32;
-      return a;
-    }
-  }
+#ifndef UNTITLED15_LIB_H
+#define UNTITLED15_LIB_H
+
+float numeroMassimo(const float numeri[],int dimensione);
+
+#endif //UNTITLED15_LIB_H
