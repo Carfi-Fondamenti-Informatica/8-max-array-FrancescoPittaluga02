8eacbe07e1ae81822a1fe5add98803f38f3fd791
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-05-08 17:47:23 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,18 @@
-
+#include "lib.h"
+bool primo(char a){
+  
+  if((a>=65 and a<=90)or(a>=97 and a<=127)){
+    return true;
+  }else{
+    return false;
+  }
+     }
+  char trasformazione(char a){
+    if(a>=65 and a<=90){
+      a=a+32;
+      return a;
+    }else if (a>=97 and a<=127){
+      a=a-32;
+      return a;
+    }
+  }
